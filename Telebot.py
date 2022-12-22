2e65158fce72180ce774aac310b85a0c54901e5c
Plotnikov VD <plotnikov.vd@students.sch2.ru>
2022-12-22 22:45:31 +0300

    Revert "Update Telebot.py"
    This reverts commit b8096a73fc495df9564c631dd1385da349b78d3a.

--- Telebot.py
+++ Telebot.py
@@ -1,8 +1,7 @@
 import telebot
 from telebot import types
-from random import random
-from json import load, dumps
-from TelegramBotData import inventory as inv
+from json import load
+from TelegramBotData import *
 BotKey = '{BotKey}'
 bot = telebot.TeleBot(BotKey)
 
@@ -36,9 +35,9 @@ def any_msg(message):
     client_id = message.chat.id
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     v = 0
-    i = inv.Inventory()
-    save[client_id] = [v, i.__dict__]
-    open("TelegramBotData/save.json", "w").write(dumps(save))
+    i = Inventory()
+    req[client_id] = [v, i.__dict__]
+    open("Save.Pre_game.json", "w").write(json.dumps(req))
     bot.send_message(message.chat.id, "Нажми на команду /restart", reply_markup=keyboard)
 
 
@@ -48,15 +47,15 @@ def any_msg(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keybord_remove = telebot.types.ReplyKeyboardRemove()
     v = 0
-    i = inv.Inventory()
-    bot.send_message(message.chat.id, text[0], reply_markup=keybord_remove)
-    for j in ways[v]:  # Добавление всех кнопок
-        if inv.go(v, j, i):
-            keyboard.add(types.KeyboardButton(text=button[v][j]))
+    i = Inventory()
+    bot.send_message(message.chat.id, texts[0], reply_markup=keybord_remove)
+    for j in l[v]:  # Добавление всех кнопок
+        if go(v, j, i):
+            keyboard.add(types.KeyboardButton(text=t[v][j]))
     # keyboard.add(types.KeyboardButton(text='/restart'))
     bot.send_message(message.chat.id, 'Что будете делать;)?', reply_markup=keyboard)
-    save[client_id] = [v, i.__dict__]
-    open("TelegramBotData/save.json", "w").write(dumps(save))
+    req[client_id] = [v, i.__dict__]
+    open("Save.Pre_game.json", "w").write(json.dumps(req))
 
 
 @bot.message_handler(content_types=["text"])
@@ -64,74 +63,74 @@ def ane_msg(message):
     client_id = message.chat.id
     keybord_remove = telebot.types.ReplyKeyboardRemove()
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    if message.text not in reverse_button.keys():
+    if message.text not in trev.keys():
         bot.send_message(message.chat.id, "Жмякай кнопки падла!", reply_markup=keyboard)
         return
-    u = reverse_button[message.text]
-    v = save[client_id][0]
-    i = inv.Inventory()
+    u = trev[message.text]
+    v = req[client_id][0]
+    i = Inventory()
     print(v, u)
-    i.__dict__ = save[client_id][1]
-    if u not in ways[v]:
+    i.__dict__ = req[client_id][1]
+    if u not in l[v]:
         bot.send_message(message.chat.id, f"Жмякай кнопки падла!", reply_markup=keyboard)
         return
-    if not inv.go(v, u, i):
+    if not go(v, u, i):
         bot.send_message(message.chat.id, "Ты не можешь это сделать!", reply_markup=keyboard)
         return
-    bot.send_message(message.chat.id, text[u], reply_markup=keybord_remove)
+    bot.send_message(message.chat.id, texts[u], reply_markup=keybord_remove)
     if u in [27, 29]:
         bot.send_message(message.chat.id, 'Пошёл нафиг, никакого продолжения', reply_markup=keybord_remove)
         keyboard.add(types.KeyboardButton(text='/restart'))
         bot.send_message(message.chat.id, 'Хочешь начать с начала?', reply_markup=keyboard)
         return
 
-    # if u == 25 or u == 31:  # Пропуск в ад
-    #    i.inventory_add(0)
-    # if u == 30:  # Получение клинка
-    #    i.inventory_add(1)
-    # if u == 17:  # Получение молитвы
-    #    i.inventory_add(2)
-    #
-    # if u == 22:  # Чтение молитвы 50%
-    #    if random() < 0.5:
-    #        keyboard.add(types.KeyboardButton(text=button[22][25]))
-    #    else:
-    #        keyboard.add(types.KeyboardButton(text=button[22][24]))
-    # elif u == 23:  # Чтение молитвы 5%
-    #    if random() < 0.95:
-    #        keyboard.add(types.KeyboardButton(text=button[23][25]))
-    #    else:
-    #        keyboard.add(types.KeyboardButton(text=button[23][24]))
-    # elif u == 18:  # Проход в комнату с клинком
-    #    if random() < 0.1 and not i.inventory_get(1):
-    #        keyboard.add(types.KeyboardButton(text=button[18][30]))
-    #    else:
-    #        keyboard.add(types.KeyboardButton(text=button[18][11]))
-    #    for j in ways[u]:
-    #        if inv.go(u, j, i) and j != 30 and j != 11:
-    #            keyboard.add(types.KeyboardButton(text=button[u][j]))
-    #            if (button[u][j] == 'Тебя не должно быть здесь'):
-    #                print(f'Error: {u=} {j=}')
-    # elif u == 8:  # Проход в комнату с клинком
-    #    if random() < 0.1 and not i.inventory_get(1):
-    #        keyboard.add(types.KeyboardButton(text=button[8][30]))
-    #    else:
-    #        keyboard.add(types.KeyboardButton(text=button[8][11]))
-    #    for j in ways[u]:
-    #        if inv.go(u, j, i) and j != 30 and j != 11:
-    #            keyboard.add(types.KeyboardButton(text=button[u][j]))
-    #            if (button[u][j] == 'Тебя не должно быть здесь'):
-    #                print(f'Error: {u=} {j=}')
+    if u == 25 or u == 31:  # Пропуск в ад
+        i.inventory_add(0)
+    if u == 30:  # Получение клинка
+        i.inventory_add(1)
+    if u == 17:  # Получение молитвы
+        i.inventory_add(2)
+
+    if u == 22:  # Чтение молитвы 50%
+        if random() < 0.5:
+            keyboard.add(types.KeyboardButton(text=t[22][25]))
+        else:
+            keyboard.add(types.KeyboardButton(text=t[22][24]))
+    elif u == 23:  # Чтение молитвы 5%
+        if random() < 0.95:
+            keyboard.add(types.KeyboardButton(text=t[23][25]))
+        else:
+            keyboard.add(types.KeyboardButton(text=t[23][24]))
+    elif u == 18:  # Проход в комнату с клинком
+        if random() < 0.1 and not i.inventory_get(1):
+            keyboard.add(types.KeyboardButton(text=t[18][30]))
+        else:
+            keyboard.add(types.KeyboardButton(text=t[18][11]))
+        for j in l[u]:
+            if go(u, j, i) and j != 30 and j != 11:
+                keyboard.add(types.KeyboardButton(text=t[u][j]))
+                if (t[u][j] == 'Тебя не должно быть здесь'):
+                    print(f'Error: {u=} {j=}')
+    elif u == 8:  # Проход в комнату с клинком
+        if random() < 0.1 and not i.inventory_get(1):
+            keyboard.add(types.KeyboardButton(text=t[8][30]))
+        else:
+            keyboard.add(types.KeyboardButton(text=t[8][11]))
+        for j in l[u]:
+            if go(u, j, i) and j != 30 and j != 11:
+                keyboard.add(types.KeyboardButton(text=t[u][j]))
+                if (t[u][j] == 'Тебя не должно быть здесь'):
+                    print(f'Error: {u=} {j=}')
     else:
-        for j in ways[u]:
-            if inv.go(u, j, i):
+        for j in l[u]:
+            if go(u, j, i):
                 # print(t[u][j])
-                keyboard.add(types.KeyboardButton(text=button[u][j]))
+                keyboard.add(types.KeyboardButton(text=t[u][j]))
     i.visit_add(u)
     # keyboard.add(types.KeyboardButton(text='/restart'))
     bot.send_message(message.chat.id, 'Что будете делать;)?', reply_markup=keyboard)
-    save[client_id] = [u, i.__dict__]
-    open("TelegramBotData/save.json", "w").write(dumps(save))
+    req[client_id] = [u, i.__dict__]
+    open("Save.Pre_game.json", "w").write(json.dumps(req))
 
 
 if __name__ == '__main__':
