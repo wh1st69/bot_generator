934833d09be25624403c36a0d128809d3a70c180
KirillVengerov76 <94645149+KirillVengerov76@users.noreply.github.com>
2022-12-23 12:38:57 +0300

    Add files via upload

--- generator1.py
+++ generator1.py
@@ -1,19 +1,26 @@
 import os
 import shutil
 import json
+
+sep = '\\' if os.name == 'nt' else '/'
 s = input('Введите адрес папки для бота:\n')
-l = ''.join(open('pattern.txt', 'r').readlines())
-l = l.format(BotKey=input('Введите ключ бота:\n'))
+BotKey = input('Введите ключ бота:\n')
+n = int(input('Введите количество вершин в графе: '))
+i = int(input('Введите число используемых полей инвенторя: '))
+cfg = ''.join(open(f'TelegramBotData{sep}config.py').readlines()).format(node=n, inventory_size=i, BotKey=BotKey)
 d = os.getcwd()
 os.chdir(s)
 if 'TelegramBotData' not in os.listdir(path='.'):
     os.mkdir('TelegramBotData')
-open('bot.py', 'w').write(l)
-open('TelegramBotData\\__init__.py', 'w').write('import TelegramBotData.inventory')
-shutil.copy(d + '\\TelegramBotData\\inventory.py', 'TelegramBotData\\inventory.py')
-shutil.copy(d + '\\TelegramBotData\\__init__.py', 'TelegramBotData\\__init__.py')
-n = int(input('Введите количество вершин в графе: '))
-open('TelegramBotData\\save.json', 'w').write('{}')
-open('TelegramBotData\\text.json', 'w').write(json.dumps([''] * n, indent=4))
-open('TelegramBotData\\ways.json', 'w').write(json.dumps([[] for _ in range(n)], indent=4))
-open('TelegramBotData\\button.json', 'w').write(json.dumps([['' for _ in range(n)] for _ in range(n)], indent=4))
+
+shutil.copy(d + f'{sep}TelegramBotData{sep}inventory.py', f'TelegramBotData{sep}inventory.py')
+shutil.copy(d + f'{sep}TelegramBotData{sep}__init__.py', f'TelegramBotData{sep}__init__.py')
+shutil.copy(d + f'{sep}pattern.py', 'bot.py')
+
+open(f'TelegramBotData{sep}config.py', 'w').write(cfg)
+open(f'TelegramBotData{sep}save.json', 'w').write(json.dumps({}, indent=4, ensure_ascii=False))
+open(f'TelegramBotData{sep}text.json', 'w').write(json.dumps([''] * n, indent=4, ensure_ascii=False))
+open(f'TelegramBotData{sep}adjacency_list.json', 'w').write(
+    json.dumps([[] for _ in range(n)], indent=4, ensure_ascii=False))
+open(f'TelegramBotData{sep}button.json', 'w').write(
+    json.dumps([['' for _ in range(n)] for _ in range(n)], indent=4, ensure_ascii=False))
