d5542606953e699e1a52ab619e2a54cbabd0e0c2
Plotnikov VD <plotnikov.vd@students.sch2.ru>
2022-12-26 21:11:53 +0300

    Теперь token лежит в папке с датой
    Надо будет потом хорошо его запрятать в логах/примерах

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import telebot
 from string import ascii_letters
 from random import randint
 
-token = ''.join(map(lambda x: chr(int(x, 16)), open('token', 'r').readlines()))
+token = ''.join(map(lambda x: chr(int(x, 16)), open('TelegramBotGeneratorData/token', 'r').readlines()))
 bot = telebot.TeleBot(token)
 
 s = ''.join([ascii_letters[randint(0, len(ascii_letters)-1)] for _ in range(randint(10, 30))])
@@ -13,7 +13,7 @@ print(f'secrete code: {s}')
 def any_msg(message):
     if message.text == s:
         bot.send_message(message.chat.id, 'Аккаунт админа подтвержден')
-        print(f'admin id: {message.chat.id}')
+        print(f'Admin id: {message.chat.id}')
         bot.stop_polling()
 
 
