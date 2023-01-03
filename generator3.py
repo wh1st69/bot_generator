2c2dd8e7c357a57dd77720318ff8fa8fa762b8c9
Plotnikov VD <plotnikov.vd@students.sch2.ru>
2023-01-03 23:13:54 +0300

    Удаление текстов вершин

--- generator3.py
+++ generator3.py
@@ -1,13 +1,22 @@
 import json
 import os
+
+
+def print_texts(texts):
+    print('Сейчас тексты для вершин графа выглядят так:')
+    for v, txt in enumerate(texts):
+        print(f'Текст для вершины {v} (индексация с нуля): \n"{txt}"')
+
+
 sep = '\\' if os.name == 'nt' else '/'
 s = input('Введите адрес папки для бота:\n') + f'{sep}TelegramBotData'
 os.chdir(s)
 text = json.load(open('text.json', 'r'))
-print('Сейчас тексты для вершин графа выглядят так:')
-for u, t in enumerate(text):
-    print(f'Текст для вершины {u} (индексация с нуля): ')
-    print(t)
+print_texts(text)
+for _ in range(int(input('Введите количество удалений, которое хотите сделать: '))):
+    u = int(input('Введите индекс вершины, текст которой хотите удалить: '))
+    text[u] = ''
+print_texts(text)
 for _ in range(int(input('Введите количество вершин в графе, тексты для которых вы хотите сейчас добавить: '))):
     u = int(input('Введите индекс вершины (индексация с нуля): '))
     t = input('Введите текст для этой вершины:\n')
