47324f1b695deaac4936b7d74226a8844ac09245
Plotnikov VD <plotnikov.vd@students.sch2.ru>
2023-01-04 16:17:42 +0300

    Комментарии теперь во втором генераторе

--- generator2.py
+++ generator2.py
@@ -1,14 +1,24 @@
-import json
-import os
+import json  # Сохранение словарей
+import os  # Переход в директорию бота
+
+# Переход в папку бота
 sep = '\\' if os.name == 'nt' else '/'
 s = input('Введите адрес папки для бота:\n') + f'{sep}TelegramBotData'
 os.chdir(s)
+
+# Считывание списка смежности
 adj_list = json.load(open('adjacency_list.json', 'r'))
+
+# Вывод списка смежности
 print('Сейчас список смежости бота выглядит так (индексация с нуля):')
 for i, l in enumerate(adj_list):
     print(f'Из вершины {i} можно попасть в вершины: ', *l)
+
+# Добавление новых ребер
 for i in range(int(input('Введите количество ребер в графе, которое вы хотите сейчас добавить: '))):
     u, v = list(map(int, input('Введите номера вершин, между которыми есть ребро (из первой во вторую) (индексация '
                                'идет с нуля): \n').split()))
     adj_list[u].append(v)
+
+# Сохранение изменений
 open('adjacency_list.json', 'w').write(json.dumps(adj_list, indent=4, ensure_ascii=False))
